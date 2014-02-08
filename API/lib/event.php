4b17cdc3ffb594d74da26d50b4779e97feab21c6
Jason El-Massih <jel-massih@hotmail.com>
2014-02-08 14:52:08 -0500

    Added events by location.  /events/:location

--- API/lib/event.php
+++ API/lib/event.php
@@ -20,4 +20,24 @@
 
 	  return json_encode($result);
 	}
+
+	function dbGetEventsByLocation($location) {
+		global $db_link;
+	  if($q = $db_link->prepare("SELECT * FROM events WHERE `location` = ?"))
+	  {
+	    $q->execute();
+	    $q->bind_result($eid,$name, $desc,$loc,$start_date, $start_time);
+
+	    while($q->fetch()) {
+	    	array_push($result, array("id"=>$eid, "event_name"=>$name, "event_desc"=>$desc, "event_loc"=>$loc,"event_start_date"=>$start_date,"event_start_time"=>$start_time));
+	    }
+
+	    if ($q->errno) {
+	      return null;
+	  	}
+	  }
+
+
+	  return json_encode($result);
+	}
 ?>
