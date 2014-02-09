4d42896c3033740a870cf1587da418287aa91f3d
Jason El-Massih <jel-massih@hotmail.com>
2014-02-09 04:07:26 -0500

    Base Layouts

---
+++ public_site/app/components/main/adminhome/messages/list/view.js
@@ -0,0 +1,28 @@
+define(function(require, exports, module) {
+  "use strict";
+
+  var app = require("app");
+  var Item = require("../item/view");
+  var Event = require("../model");
+
+  var Layout = Backbone.Layout.extend({
+    template: require("ldsh!./template"),
+    tagName: "div",
+    className: "list-group",
+    serialize: function() {
+      return {events: this.collection};
+    },
+    beforeRender: function() {
+     this.collection.each(function(my_event) {
+        this.insertView("#listInsertPoint", new Item({
+          model:my_event
+        }));
+      }, this);
+    },
+    initialize: function() {
+      this.listenTo(this.collection, "reset sync request", this.render);
+    }
+  });
+
+  module.exports = Layout;
+});
